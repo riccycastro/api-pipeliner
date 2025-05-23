a1dfa3eb34c44261f7d772785788ec6792a8ae64
rcastro <cruzcastro07@gmail.com>
2025-05-23 16:52:35 +0100

    improvements

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ if (fssync.existsSync('.env.local')) {
     console.log('.env.local not found, skipping.')
 }
 
+const crypto = require('crypto')
+
+console.log(
+    crypto
+        .createHmac('sha256', '7cf2012c-967a-4e2e-9827-ac508432db4e')
+        .update('9827' + Math.floor(Date.now() / 1000) + '{"action":"restart-middleware","target":"acount-middleware","triggered_by":"riccycastro","options":{"environment":"dev","branch_or_tag":"master","composer":"Yes","migration":"Yes"}}')
+        .digest('hex'),
+    Math.floor(Date.now() / 1000)
+)
+
 const createRateLimitMiddleware = require('./middlewares/rate-limit-middleware')
 const securityMiddleware = require('./middlewares/security-middleware.js')
 
@@ -38,6 +48,8 @@ app.use(securityMiddleware)
 
 // --- Submit a Job ---
 app.post('/webhook', createRateLimitMiddleware(), async (req, res) => {
+    console.log(req.body)
+
     // Directories for job data and logs
     const JOBS_DIR = getJobsDir()
 
